616fffe5cccf962e955a8ebd38821962b7b65177
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-20 23:45:58 +0300

    feat: implement sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  
+
+  for (let i = 1; i < matrix.length; i += 2) {
+    matrix[i].reverse();
+  };
+
+  const toFlat = matrix.reduce((accumulator, current) => {
+    return accumulator.concat(current);
+  });
+  return toFlat;
 }
+
