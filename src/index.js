b9c26d5125ed2f9bf53dea9f60ad5d2342b5a4f3
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-20 23:48:33 +0300

    fix: fix exceptions

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-  
+  if (matrix === undefined || matrix.length === 0) {
+    return [];
+  }
 
   for (let i = 1; i < matrix.length; i += 2) {
     matrix[i].reverse();
@@ -13,4 +15,3 @@ module.exports = function towelSort(matrix) {
   });
   return toFlat;
 }
-
